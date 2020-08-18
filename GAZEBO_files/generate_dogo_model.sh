31fa04e13f681bb3cd29d1e9ec4c0a1435ec495d
adev <devillard@isir.upmc.fr>
2020-08-18 11:55:37 +0200

    updated

--- GAZEBO_files/generate_dogo_model.sh
+++ GAZEBO_files/generate_dogo_model.sh
@@ -9,5 +9,15 @@ echo ""
 
 # Create sdf model
 (cat $MODEL_PARAM && cat functions.erb && cat dogo.sdf.erb) | erb -T 1 > $MODEL_NAME.sdf #file.sdf
+(cat $MODEL_PARAM && cat functions.erb && cat dogo.config.erb) | erb -T 1 > $MODEL_NAME.config #file.config
+mkdir -p ~/.gazebo/models/dogo
+cp $MODEL_NAME.sdf ~/.gazebo/models/dogo/$MODEL_NAME.sdf
+cp $MODEL_NAME.config ~/.gazebo/models/dogo/$MODEL_NAME.config
+
+cp ../STL_files/leg0.STL ~/.gazebo/models/dogo/full_leg0.STL
+cp ../STL_files/full_leg1.STL ~/.gazebo/models/dogo/full_leg1.STL
+cp ../STL_files/full_leg2.STL ~/.gazebo/models/dogo/full_leg2.STL
+cp ../STL_files/full_leg3.STL ~/.gazebo/models/dogo/full_leg3.STL
+
 echo "[INFO] model.sdf file created. ($MODEL_NAME.sdf)"
 
